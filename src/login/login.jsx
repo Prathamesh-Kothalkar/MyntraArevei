5edc85ae5ca829dcbbec160ef990dff60e0e2e09
Varun Pandey <varunp5324@gmail.com>
2024-08-24 23:57:54 +0530

    Varun's Part

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,3 +1,4 @@
+import 'animate.css';
 import { Link } from "react-router-dom";
 import { useRef, useState } from 'react';
 // import logo from "MyntraArevei\src\assets\newlogo.jpeg";
@@ -17,33 +18,33 @@ export default function Login() {
         <>
             <div className="loginContainer">
                 <div className="loginBanner">
-                    <div className="loginText">Login</div>
-                    <div className="card1">
+                    <div className="loginText animate__animated animate__bounce animate__delay-0.5s">Login</div>
+                    <div className="card1 animate__animated animate__fadeInUp">
                         {/* <img src="https://i.ibb.co/WDcpcNh/newlogo.jpg" alt="logo" /> */}
                         <img src="https://i.ibb.co/ZSQC38p/newlogo-removebg-preview.png" alt="logo" />
                         <form action="">
                             <div className="inputdiv">
-                                <label htmlFor="Email">
+                                <label htmlFor="Email" className='animate__animated animate__fadeInLeft  animate__delay-1s'>
                                     Email: &nbsp;&nbsp;&nbsp;
-                                    <input type="email" name="Email" id="Email" placeholder='abc@xxx.xom' />
+                                    <input type="email" name="Email" id="Email" placeholder='abc@xxx.xom'  className='animate__animated animate__fadeInRightBig  animate__delay-1s'/>
                                 </label>
                             </div>
                             <div className="inputdiv">
-                                <label htmlFor="pass">
+                                <label htmlFor="pass" className='animate__animated animate__fadeInLeft  animate__delay-1s'>
                                     Password: &nbsp;&nbsp;&nbsp;
-                                    <input type="password" name="pass" id="pass" ref={reftype} placeholder='****'/>
+                                    <input type="password" name="pass" id="pass" ref={reftype} placeholder='****' className='animate__animated animate__fadeInRightBig  animate__delay-1s'/>
                                 </label>
                             </div>
-                            <div className="inputdiv">
+                            <div className="inputdiv  animate__animated animate__zoomIn animate__delay-1s ">
                                 <div className='tandc'>By continuing, i agree to the <strong>Terms and condtition</strong></div>
                             </div>
-                            <div className="inputdiv">
+                            <div className="inputdiv animate__animated animate__heartBeat animate__delay-1s animate__repeat-2">
                                 <div className='lookbtn borderleft' onClick={handleClick}>
                                     <i class="fa-solid fa-eye"></i>
                                 </div>
                                 <button type="submit" className='submitbtn'>Continue</button>
                             </div>
-                            <div className="inputdiv">
+                            <div className="inputdiv  animate__animated animate__zoomIn animate__delay-1s ">
                                 <div className='tandc'>Not having an account <strong><Link to="/signup">SIGN-IN</Link></strong></div>
                             </div>
                         </form>
--- src/signUp/signUp.jsx
+++ src/signUp/signUp.jsx
@@ -1,3 +1,4 @@
+import 'animate.css';
 import { Link } from "react-router-dom";
 import { useRef, useState } from 'react';
 // import logo from "MyntraArevei\src\assets\newlogo.jpeg";
@@ -17,46 +18,46 @@ export default function SignUp() {
         <>
             <div className="loginContainer">
                 <div className="loginBanner">
-                    <div className="loginText">Signup</div>
-                    <div className="card1">
+                    <div className="loginText animate__animated animate__bounce animate__delay-0.5s">Signup</div>
+                    <div className="card1 animate__animated animate__fadeInUp">
                         {/* <img src="https://i.ibb.co/WDcpcNh/newlogo.jpg" alt="logo" /> */}
                         <img src="https://i.ibb.co/ZSQC38p/newlogo-removebg-preview.png" alt="logo" />
                         <form action="">
                             <div className="inputdiv">
-                                <label htmlFor="Name">
+                                <label htmlFor="Name" className='animate__animated animate__fadeInLeft  animate__delay-1s'>
                                     Name: &nbsp;&nbsp;&nbsp;
-                                    <input type="text" name="Name" id="Name" placeholder='Full Name' />
+                                    <input type="text" name="Name" id="Name" placeholder='Full Name' className='animate__animated animate__fadeInRightBig  animate__delay-1s'/>
                                 </label>
                             </div>
                             <div className="inputdiv">
-                                <label htmlFor="phone">
+                                <label htmlFor="phone" className='animate__animated animate__fadeInLeft  animate__delay-1s'>
                                     Phone: &nbsp;&nbsp;&nbsp;
-                                    <input type="text" name="phone" id="phone" placeholder='+91' />
+                                    <input type="text" name="phone" id="phone" placeholder='+91'  className='animate__animated animate__fadeInRightBig  animate__delay-1s'/>
                                 </label>
                             </div>
                             <div className="inputdiv">
-                                <label htmlFor="Email">
+                                <label htmlFor="Email" className='animate__animated animate__fadeInLeft  animate__delay-1s'>
                                     Email: &nbsp;&nbsp;&nbsp;
-                                    <input type="email" name="Email" id="Email" placeholder='abc@xxx.xom' />
+                                    <input type="email" name="Email" id="Email" placeholder='abc@xxx.xom'  className='animate__animated animate__fadeInRightBig  animate__delay-1s'/>
                                 </label>
                             </div>
                             <div className="inputdiv">
-                                <label htmlFor="pass">
+                                <label htmlFor="pass" className='animate__animated animate__fadeInLeft  animate__delay-1s'>
                                     Password: &nbsp;&nbsp;&nbsp;
-                                    <input type="password" name="pass" id="pass" ref={reftype} placeholder='****' />
+                                    <input type="password" name="pass" id="pass" ref={reftype} placeholder='****'  className='animate__animated animate__fadeInRightBig  animate__delay-1s'/>
                                 </label>
                             </div>
-                            <div className="inputdiv">
+                            <div className="inputdiv  animate__animated animate__zoomIn animate__delay-1s ">
                                 <div className='tandc'>By continuing, i agree to the <strong>Terms and condtition</strong></div>
                             </div>
-                            <div className="inputdiv">
+                            <div className="inputdiv animate__animated animate__heartBeat animate__delay-1s animate__repeat-2">
                                 <div className='lookbtn borderleft' onClick={handleClick}>
                                     <i class="fa-solid fa-eye"></i>
                                 </div>
                                 <button type="submit" className='submitbtn'>Continue</button>
                             </div>
                             <div className="inputdiv">
-                                <div className='tandc'>Go to <strong><Link to="/">Home</Link></strong></div>
+                                <div className='tandc animate__animated animate__zoomIn animate__delay-0.5s'>Go to <strong><Link to="/">Home</Link></strong></div>
                             </div>
                         </form>
                     </div>
