21aefa2acca57e203136c01bb2318e232e598eff
Varunp01 <152736714+Varunp01@users.noreply.github.com>
2024-08-24 16:16:05 +0530

    Update login.jsx

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,44 +1,49 @@
+import { Link } from "react-router-dom";
 import { useRef, useState } from 'react';
 import './login.css'
-export default function Login(){
+export default function Login() {
     let reftype = useRef();
     let handleClick = () => {
         // ref.current.style.backgroundColor = "black";
-        if (reftype.current.type=="text" && reftype.current.value!="") {
-            reftype.current.type="password";
-        }else if(reftype.current.type=="password" && reftype.current.value!="") {
-            reftype.current.type="text";
+        if (reftype.current.type == "text" && reftype.current.value != "") {
+            reftype.current.type = "password";
+        } else if (reftype.current.type == "password" && reftype.current.value != "") {
+            reftype.current.type = "text";
         }
     };
-  return(
+    return (
         <>
             <div className="loginContainer">
                 <div className="loginBanner">
                     <div className="loginText">Login</div>
-                    <div className="cards">
-                        <div className="card1">
-                            <form action="">
-                                <div className="inputdiv">
+                    <div className="card1">
+                        <img src="src\assets\clogo.jpeg" alt="logo" />
+                        <form action="">
+                            <div className="inputdiv">
                                 <label htmlFor="Email">
                                     Email: &nbsp;&nbsp;&nbsp;
-                                <input type="email" name="Email" id="Email" placeholder='abc@xxx.xom'/>
+                                    <input type="email" name="Email" id="Email" placeholder='abc@xxx.xom' />
                                 </label>
-                                </div><br></br>
-                                <div className="inputdiv">
+                            </div>
+                            <div className="inputdiv">
                                 <label htmlFor="pass">
                                     Password: &nbsp;&nbsp;&nbsp;
-                                <input type="password" name="pass" id="pass" ref={reftype}/>
+                                    <input type="password" name="pass" id="pass" ref={reftype} placeholder='****'/>
                                 </label>
-                                </div><br></br>
-                                <div className="inputdiv">
+                            </div>
+                            <div className="inputdiv">
+                                <div className='tandc'>By continuing, i agree to the <strong>Terms and condtition</strong></div>
+                            </div>
+                            <div className="inputdiv">
                                 <div className='lookbtn borderleft' onClick={handleClick}>
-                                {/* { {seeing}=="true" ? (<i class="fa-solid fa-eye"></i>) : (<i class="fa-solid fa-eye-slash"></i>) } */}
-                                <i class="fa-solid fa-eye"></i>
+                                    <i class="fa-solid fa-eye"></i>
                                 </div>
-                                <button type="submit" className='submitbtn'>Login</button>
-                                </div><br></br>
-                            </form>
-                        </div>
+                                <button type="submit" className='submitbtn'>Continue</button>
+                            </div>
+                            <div className="inputdiv">
+                                <div className='tandc'>Not having an account <strong><Link to="/signup">SIGN-IN</Link></strong></div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
