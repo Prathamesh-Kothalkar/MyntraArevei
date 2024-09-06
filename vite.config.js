51b36888ea351071d0b488956364dfe87d02a81c
Varun Pandey <varunp5324@gmail.com>
2024-09-06 17:28:47 +0530

    viteConfig

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const server = import.meta.env.VITE_BACKEND_SERVER;
+
+// const server = import.meta.env.VITE_BACKEND_SERVER;
 // https://vitejs.dev/config/
 export default defineConfig({
   // server:{
   //   proxy:{
+  //     "/api/vi": "http://localhost:3000"
   //     "/vi": `${server}`
   //   }
   // },
